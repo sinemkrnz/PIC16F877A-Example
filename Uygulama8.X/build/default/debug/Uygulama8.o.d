4ca404b2af76d5fae4bba441bf2800285ad0d313
Sinem Kurnaz <sinemkrnz96@gmail.com>
2019-10-29 21:54:22 +0300

    Uygulama8 eklendi.
    İki sayıyı bölüp, bölümü Port D'de gösteren uygulama.

---
+++ Uygulama8.X/build/default/debug/Uygulama8.o.d
@@ -0,0 +1,3 @@
+build/default/debug/Uygulama8.o:  \
+	C:/Users/Sinem/Desktop/Projeler/Uygulama8.X/Uygulama8.asm  \
+
---
+++ Uygulama8.X/build/default/production/Uygulama8.o.d
@@ -0,0 +1,3 @@
+build/default/production/Uygulama8.o:  \
+	C:/Users/Sinem/Desktop/Projeler/Uygulama8.X/Uygulama8.asm  \
+
