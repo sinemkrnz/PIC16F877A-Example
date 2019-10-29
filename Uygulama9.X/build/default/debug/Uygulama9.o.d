586b66c7deee1eb92412075348c0d3eabf19e65d
Sinem Kurnaz <sinemkrnz96@gmail.com>
2019-10-30 00:02:07 +0300

    Uygulama9 eklendi.
    Port A'nın 0. pinine bağlı butona basıldığında Port B'ye bağlı LEDleri (0-9) ileri saydıran, Port A'nın 1.pinine bağlı butona basıldığında Port B'ye bağlı butonları (9-0) geri saydıran program.

---
+++ Uygulama9.X/build/default/debug/Uygulama9.o.d
@@ -0,0 +1,3 @@
+build/default/debug/Uygulama9.o:  \
+	C:/Users/Sinem/Desktop/Projeler/Uygulama9.X/Uygulama9.asm  \
+
---
+++ Uygulama9.X/build/default/production/Uygulama9.o.d
@@ -0,0 +1,3 @@
+build/default/production/Uygulama9.o:  \
+	C:/Users/Sinem/Desktop/Projeler/Uygulama9.X/Uygulama9.asm  \
+
