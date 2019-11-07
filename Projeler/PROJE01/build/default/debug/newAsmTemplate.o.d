6b7ec5c904b85c076683cc48bbba15507d3e76f0
Sinem Kurnaz <sinemkrnz96@gmail.com>
2019-11-07 19:10:02 +0300

    Proje 1 eklendi
    12 Bitlik ileri geri sayıcı.

---
+++ Projeler/PROJE01/build/default/debug/newAsmTemplate.o.d
@@ -0,0 +1,3 @@
+build/default/debug/newAsmTemplate.o:  \
+	C:/Users/Sinem/Desktop/Mikroislemciler_Odevler/Odev_1.X/newAsmTemplate.asm  \
+
---
+++ Projeler/PROJE01/build/default/production/newAsmTemplate.o.d
@@ -0,0 +1,3 @@
+build/default/production/newAsmTemplate.o:  \
+	C:/Users/Sinem/Desktop/Mikroislemciler_Odevler/Odev_1.X/newAsmTemplate.asm  \
+
